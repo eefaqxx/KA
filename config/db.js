a585e66f012f5b42f7c4873c8d92b7ea0a78755e
eefaqxx <faqihputraaaa@gmail.com>
2024-02-05 23:51:04 +0800

    first commit

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
-	host: 'sql.freedb.tech',
-	dialect: 'mysql',
-    username: 'freedb_pakisenju',
-    password: 'Fpr&Wt2#Kaau*23',
-    database: 'freedb_sequelize-todo'
+  host: "sql.freedb.tech",
+  dialect: "mysql",
+  username: "freedb_eefaqxx",
+  password: "*FcET?gHg73qDs7",
+  database: "freedb_freedb_cc",
 });
 
-module.exports = sequelize
+module.exports = sequelize;
